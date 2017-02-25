6807fdb7e1875f9d07e864ea7cfdd377349a6e88
marnow955 <marnow955@gmail.com>
2017-02-25 19:03:21 +0100

    Center images in StackPanels, fileFilters, open and save image

--- src/gui/javafx/Controller.java
+++ src/gui/javafx/Controller.java
@@ -7,14 +7,17 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import photo.processor.*;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
 
 /**
  * Created by marek on 2017-02-14.
@@ -22,6 +25,13 @@ import java.io.File;
 public class Controller {
 
     private boolean isImageSelected = false;
+    FileChooser.ExtensionFilter[] filters = {
+            new FileChooser.ExtensionFilter("Image (*.png, *.jpg, *.gif)", "*.png", "*.jpg", "*.gif", "*.jpeg",
+                    "*.PNG", "*.JPG", "*.GIF", "*.JPEG"),
+            new FileChooser.ExtensionFilter(".png", "*.png", "*.PNG"),
+            new FileChooser.ExtensionFilter(".jpg", "*.jpg", "*.JPG", "*.jpeg", "*.JPEG"),
+            new FileChooser.ExtensionFilter(".gif", "*.gif", "*.GIF")
+    };
 
     private Stage window;
     private Image beforeImg;
@@ -31,10 +41,8 @@ public class Controller {
 
     public ScrollPane leftSP;
     public ScrollPane rightSP;
-    public VBox center;
-    public ChoiceBox algorithmChoiceBox;
-    public ChoiceBox paletteChoiceBox;
-    public Button submitButton;
+    public StackPane leftStackPImg;
+    public StackPane rightStackPImg;
     public ImageView beforeImageView;
     public ImageView afterImageView;
 
@@ -45,10 +53,20 @@ public class Controller {
     public RadioButton afterOSizeRB;
     public RadioButton afterFitToWRB;
 
+    public VBox center;
+    public ChoiceBox algorithmChoiceBox;
+    public ChoiceBox paletteChoiceBox;
+    public Button submitButton;
+
     public void setStageAndSetupView(Stage primaryStage) {
         window = primaryStage;
         leftSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
+        leftStackPImg.prefWidthProperty().bind(leftSP.widthProperty().subtract(10));
+        leftStackPImg.prefHeightProperty().bind(leftSP.heightProperty().subtract(10));
         rightSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
+        rightStackPImg.prefWidthProperty().bind(rightSP.widthProperty().subtract(10));
+        rightStackPImg.prefHeightProperty().bind(rightSP.heightProperty().subtract(10));
+
         paletteChoiceBox.prefWidthProperty().bind(algorithmChoiceBox.widthProperty());
         submitButton.prefWidthProperty().bind(algorithmChoiceBox.widthProperty());
 
@@ -61,8 +79,6 @@ public class Controller {
         afterOSizeRB.setToggleGroup(afterTG);
         afterFitToWRB.setToggleGroup(afterTG);
         afterFitToWRB.setSelected(true);
-//        afterOSizeRB.layoutXProperty().bind(rightSP.layoutXProperty());
-        afterOSizeRB.setLayoutX(rightSP.getLayoutX());
 
         algorithmChoiceBox.getItems().addAll(
                 "Nearest Color",
@@ -80,15 +96,47 @@ public class Controller {
 
     public void openFile(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter(".png, .jpg, .jpeg", "*.png", "*.jpg",
-                "*.jpeg"));
+        fileChooser.getExtensionFilters().addAll(filters);
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
         File file = fileChooser.showOpenDialog(window);
         if (file != null) {
-            isImageSelected = true;
-            beforeImg = new Image(file.toURI().toString());
-            displayBeforeImage();
+            if (checkFileExtension(file)) {
+                isImageSelected = true;
+                beforeImg = new Image(file.toURI().toString());
+                displayBeforeImage();
+            } else {
+                new Alert(Alert.AlertType.ERROR, file.getName() + " has no valid file-extension").show();
+            }
         }
+    }
+
+    public void saveFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(filters);
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        fileChooser.setInitialFileName("*.png");
+        File file = fileChooser.showSaveDialog(window);
+        if (file != null) {
+            try {
+                if (checkFileExtension(file)){
+                    ImageIO.write(SwingFXUtils.fromFXImage(afterImg, null), file.getName().substring(
+                            file.getName().lastIndexOf(".")+1), file);
+                } else {
+                    new Alert(Alert.AlertType.ERROR, file.getName() + " has no valid file-extension").show();
+                }
+            } catch (IOException e) {
+                new Alert(Alert.AlertType.ERROR, String.format("Cannot save file %s", file.getPath())).show();
+            }
+        }
+    }
+
+    private boolean checkFileExtension(File file) {
+        for (String filter: filters[0].getExtensions()) {
+            if (file.getName().endsWith(filter.substring(1))) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void displayBeforeImage() {
