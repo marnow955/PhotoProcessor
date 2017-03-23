9f3cea4a451fd3b54bd92bc13f2091f2036670d7
marnow955 <marnow955@gmail.com>
2017-03-23 23:47:18 +0100

    Rebuild of user interface: One main image window with floating left-side original img. Code refactoring.

--- src/gui/javafx/Controller.java
+++ src/gui/javafx/Controller.java
@@ -1,14 +1,15 @@
 package gui.javafx;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.embed.swing.SwingFXUtils;
-import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import photo.processor.*;
@@ -17,15 +18,15 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
 
 /**
  * Created by marek on 2017-02-14.
  */
 public class Controller {
 
-    private boolean isImageSelected = false;
-    FileChooser.ExtensionFilter[] filters = {
+    private BooleanProperty isImageSelected = new SimpleBooleanProperty(false);
+//    private boolean isImageSelected = false;
+    private FileChooser.ExtensionFilter[] filters = {
             new FileChooser.ExtensionFilter("Image (*.png, *.jpg, *.gif)", "*.png", "*.jpg", "*.gif", "*.jpeg",
                     "*.PNG", "*.JPG", "*.GIF", "*.JPEG"),
             new FileChooser.ExtensionFilter(".png", "*.png", "*.PNG"),
@@ -34,51 +35,38 @@ public class Controller {
     };
 
     private Stage window;
-    private Image beforeImg;
-    private Image afterImg;
+    private Image originalImg;
+    private Image mainImg;
     private PhotoProcessor processor;
     private Color[] colorPalette;
 
+    public HBox leftHBox;
     public ScrollPane leftSP;
-    public ScrollPane rightSP;
+    public ScrollPane mainSP;
     public StackPane leftStackPImg;
-    public StackPane rightStackPImg;
-    public ImageView beforeImageView;
-    public ImageView afterImageView;
+    public StackPane mainStackPImg;
+    public ImageView originalImageView;
+    public ImageView mainImageView;
+    public Button leftPanelButton;
 
-    public ToggleGroup beforeTG;
-    public RadioButton beforeOSizeRB;
-    public RadioButton beforeFitToWRB;
-    public ToggleGroup afterTG;
-    public RadioButton afterOSizeRB;
-    public RadioButton afterFitToWRB;
-
-    public VBox center;
-    public ChoiceBox algorithmChoiceBox;
-    public ChoiceBox paletteChoiceBox;
+    public ChoiceBox<String> algorithmChoiceBox;
+    public ChoiceBox<String> paletteChoiceBox;
     public Button submitButton;
 
-    public void setStageAndSetupView(Stage primaryStage) {
+    void setStageAndSetupView(Stage primaryStage) {
         window = primaryStage;
-        leftSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
+//        leftSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
         leftStackPImg.prefWidthProperty().bind(leftSP.widthProperty().subtract(10));
         leftStackPImg.prefHeightProperty().bind(leftSP.heightProperty().subtract(10));
-        rightSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
-        rightStackPImg.prefWidthProperty().bind(rightSP.widthProperty().subtract(10));
-        rightStackPImg.prefHeightProperty().bind(rightSP.heightProperty().subtract(10));
+//        rightSP.prefWidthProperty().bind(window.widthProperty().subtract(center.getPrefWidth()+70).divide(2));
+        mainStackPImg.prefWidthProperty().bind(mainSP.widthProperty().subtract(10));
+        mainStackPImg.prefHeightProperty().bind(mainSP.heightProperty().subtract(10));
+        leftPanelButton.prefHeightProperty().bind(leftHBox.heightProperty());
+        leftPanelButton.disableProperty().bind(isImageSelected.not());
+        leftSP.managedProperty().bind(leftSP.visibleProperty());
 
         paletteChoiceBox.prefWidthProperty().bind(algorithmChoiceBox.widthProperty());
         submitButton.prefWidthProperty().bind(algorithmChoiceBox.widthProperty());
-
-        beforeTG = new ToggleGroup();
-        beforeOSizeRB.setToggleGroup(beforeTG);
-        beforeFitToWRB.setToggleGroup(beforeTG);
-        beforeFitToWRB.setSelected(true);
-
-        afterTG = new ToggleGroup();
-        afterOSizeRB.setToggleGroup(afterTG);
-        afterFitToWRB.setToggleGroup(afterTG);
-        afterFitToWRB.setSelected(true);
 
         algorithmChoiceBox.getItems().addAll(
                 "Nearest Color",
@@ -94,23 +82,26 @@ public class Controller {
         paletteChoiceBox.setValue("27");
     }
 
-    public void openFile(ActionEvent actionEvent) {
+    public void openFile() {
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(filters);
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
         File file = fileChooser.showOpenDialog(window);
         if (file != null) {
             if (checkFileExtension(file)) {
-                isImageSelected = true;
-                beforeImg = new Image(file.toURI().toString());
-                displayBeforeImage();
+                isImageSelected.set(true);
+                originalImg = new Image(file.toURI().toString());
+                mainImg = originalImg;
+                displayOriginalImage();
+                displayMainImage();
             } else {
                 new Alert(Alert.AlertType.ERROR, file.getName() + " has no valid file-extension").show();
             }
         }
     }
 
-    public void saveFile(ActionEvent actionEvent) {
+    public void saveFile() {
+//        TODO: check if is file to save
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(filters);
         fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
@@ -119,7 +110,7 @@ public class Controller {
         if (file != null) {
             try {
                 if (checkFileExtension(file)){
-                    ImageIO.write(SwingFXUtils.fromFXImage(afterImg, null), file.getName().substring(
+                    ImageIO.write(SwingFXUtils.fromFXImage(mainImg, null), file.getName().substring(
                             file.getName().lastIndexOf(".")+1), file);
                 } else {
                     new Alert(Alert.AlertType.ERROR, file.getName() + " has no valid file-extension").show();
@@ -139,27 +130,27 @@ public class Controller {
         return false;
     }
 
-    public void displayBeforeImage() {
-        if (isImageSelected)
-            beforeImageView.setImage(beforeImg);
+    private void displayOriginalImage() {
+        if (isImageSelected.get())
+            originalImageView.setImage(originalImg);
     }
 
-    public void displayAfterImage() {
-        afterImageView.setImage(afterImg);
+    private void displayMainImage() {
+        mainImageView.setImage(mainImg);
     }
 
-    public void mainProcessSubmitted(ActionEvent actionEvent) {
-        if (isImageSelected) {
+    public void mainProcessSubmitted() {
+        if (isImageSelected.get()) {
             selectPhotoProcessor();
             selectColorPalette();
-            afterImg = SwingFXUtils.toFXImage(processor.getTransformedImage(SwingFXUtils.fromFXImage(beforeImg, null),
+            mainImg = SwingFXUtils.toFXImage(processor.getTransformedImage(SwingFXUtils.fromFXImage(originalImg, null),
                     colorPalette), null);
-            displayAfterImage();
+            displayMainImage();
         }
     }
 
     private void selectColorPalette() {
-        switch (paletteChoiceBox.getSelectionModel().getSelectedItem().toString()) {
+        switch (paletteChoiceBox.getSelectionModel().getSelectedItem()) {
             case "2": colorPalette = PhotoProcessor.TWO_COLORS_PALETTE; break;
             case "8": colorPalette = PhotoProcessor.EIGHT_COLORS_PALETTE; break;
             case "27": colorPalette = PhotoProcessor.TWENTY_SEVEN_COLORS_PALETTE; break;
@@ -167,7 +158,7 @@ public class Controller {
     }
 
     private void selectPhotoProcessor() {
-        switch (algorithmChoiceBox.getSelectionModel().getSelectedItem().toString()) {
+        switch (algorithmChoiceBox.getSelectionModel().getSelectedItem()) {
             case "Nearest Color": processor = new NearestColor(); break;
             case "NC with Noise Signal": processor = new SignalNoiseApproximation(); break;
             case "Floyd-Steinberg Dithering": processor = new FloydSteinbergDithering(); break;
@@ -177,7 +168,11 @@ public class Controller {
         }
     }
 
-    public void closeProgram(ActionEvent actionEvent) {
+    public void closeProgram() {
         window.close();
+    }
+
+    public void showLeftPanel() {
+        leftSP.setVisible(!leftSP.isVisible());
     }
 }
